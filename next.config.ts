38ec47032f4d85b1b6ebc9711be174eada18b5a6
tkkserviceteam <tkkserviceteam@gmail.com>
2026-05-15 08:00:29 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/internal-crm/:path*',
+        destination: 'https://211.75.18.228/:path*', // 你的 CRM 位址
+      },
+    ];
+  },
+};
 export default nextConfig;
