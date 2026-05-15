e38e83137296a5a5e80a22b19c9739b8eed17994
tkkserviceteam <tkkserviceteam@gmail.com>
2026-05-15 16:06:12 +0800

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/internal-crm/:path*',
-        destination: 'https://211.75.18.228/:path*', // 你的 CRM 位址
+        // 當你訪問 /crm-api/xxx 時，實際上是去抓 211.75.18.228 的內容
+        source: '/crm-api/:path*',
+        destination: 'http://211.75.18.228/:path*', 
       },
     ];
   },
+  // 如果你有其他原本的設定（如 images），請加在下面
 };
 
 export default nextConfig;
